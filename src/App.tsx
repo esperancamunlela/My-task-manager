037c86dc32219336882ccdbcedf5f96f721f0d88
esperancamunlela <esperancamunlela@gmail.com>
2025-05-26 03:11:08 +0200

    create a delete option in the tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,68 @@
-import { PlusCircleIcon } from "@phosphor-icons/react";
+import { PlusCircleIcon, TrashSimpleIcon } from "@phosphor-icons/react";
 import { useState } from "react";
 import { Footer, Header } from "./headAndFooter";
 
-
-
 export const App = () => {
-
-const [addTask, setaddTask] = useState("");
+  const [addTask, setaddTask] = useState("");
   const [listTasks, setListTasks] = useState<string[]>([]);
 
   function handleAddTask(event: React.FormEvent) {
     event.preventDefault();
-    if (addTask.trim() === "") {return}else{
-    setListTasks([...listTasks, addTask]);
-    setaddTask("");
-  }}
+    if (addTask.trim() === "") {
+      return;
+    } else {
+      setListTasks([...listTasks, addTask]);
+      setaddTask("");
+    }
+  }
+
+  function deleteTask(indice: number) {
+    const novaLista = listTasks.filter((task, i) => i !== indice);
+    setListTasks(novaLista);
+  }
+
   return (
     <>
       <div className="flex flex-col p-auto h-screen">
-        <Header/>
+        <Header />
 
         <main className="flex flex-col flex-1 pl-12 pr-4 py-2 bg-fuchsia-200 items-center">
-                <p className="text-purple-950">
-              Que tarefa temos a cumprir?
-            </p>
-          <form onSubmit={handleAddTask}  className="flex ">
+          <p className="text-purple-950">Que tarefa temos a cumprir?</p>
+          <form onSubmit={handleAddTask} className="flex ">
             <input
               type="text"
-             value={addTask}
-            onChange={(e) => setaddTask(e.target.value)}
+              value={addTask}
+              onChange={(e) => setaddTask(e.target.value)}
               placeholder="Adicione aqui uma task..."
-              className=" h-5 w-50 border rounded-sm text-purple-950 bg-fuchsia-50 text-xs"
+              className=" h-5 w-50 border rounded-sm text-purple-950 bg-fuchsia-50 text-xs "
             />
-            <button type="submit" >
-            <PlusCircleIcon size={20} weight="duotone" className="-ml-5 text-purple-950 cursor-pointer" />
+            <button type="submit">
+              <PlusCircleIcon
+                size={20}
+                weight="duotone"
+                className="-ml-5 text-purple-950 cursor-pointer"
+              />
             </button>
           </form>
-
-          <div className="w-75 max-w-md flex flex-col gap-1 mt-3">
-          {listTasks.map((task, indice) => (
-            <div key={indice} className="bg-fuchsia-100 text-purple-950 text-xs p-2 rounded">
-              {task}
-            </div>
-          ))}
-        </div>
+          <div className="w-75 max-w-md flex flex-col gap-1 mt-3 ">
+            {listTasks.map((task, indice) => (
+              <div
+                key={indice}
+                className="bg-fuchsia-100 text-purple-950 text-xs p-2 rounded hover:bg-fuchsia-400 flex justify-between"
+              >
+                {task}
+                <button
+                  className="cursor-pointer hover:text-red-800 font-bold"
+                  onClick={() => deleteTask(indice)}
+                >
+                  <TrashSimpleIcon size={16} weight="bold" />
+                </button>
+              </div>
+            ))}
+          </div>
         </main>
-        
- <Footer/>
+
+        <Footer />
       </div>
     </>
   );
